0107f86f42877f0ed6ebdb65c9efe176d925a3fb
Timothy Pocorni <timmy1420@users.noreply.github.com>
2019-06-27 00:01:14 -0300

    Added files

---
+++ README.md
@@ -0,0 +1,9 @@
+# Note to self
+- Laravel queues test with Redis
+
+# ENV variables to use
+- QUEUE_CONNECTION=redis
+
+
+___
+This is just a minimal test. Use for quick testing or just checking how queueing works :-)
---
+++ app/Jobs/ActionJob.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use App\Action;
+
+class ActionJob implements ShouldQueue {
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct() {}
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle() {
+        sleep(20);
+        Action::create(['action' => 'Do-er']);
+    }
+}
